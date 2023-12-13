ed198596d8c0804c29a5a26597e0503f1d1911c0
Martin Brosset <martin.brosset@student-cs.fr>
2023-12-13 18:22:48 +0100

    test 2

---
+++ main.py
@@ -0,0 +1,170 @@
+import numpy as np
+import torch
+from models.seq2seq import EncoderRNN, DecoderRNN, Net_GRU
+from loss.dilate_loss import dilate_loss
+from torch.utils.data import DataLoader, Dataset
+import random
+import pandas as pd
+from tslearn.metrics import dtw, dtw_path
+import matplotlib.pyplot as plt
+import warnings
+import warnings; warnings.simplefilter('ignore')
+import os
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+random.seed(0)
+
+# parameters
+batch_size = 100
+N = 500
+N_input = 20
+N_output = 20  
+sigma = 0.01
+gamma = 0.01
+
+# Load synthetic dataset
+DATA_PATH = "../data/"
+
+ecg_train = np.array(pd.read_table(DATA_PATH + "ECG5000/ECG5000_TRAIN.tsv"))[:, :, np.newaxis]
+ecg_test = np.array(pd.read_table(DATA_PATH + "ECG5000/ECG5000_TEST.tsv"))[:, :, np.newaxis]
+
+print(ecg_train.shape, ecg_test.shape)
+
+class ECG5000Dataset(Dataset):
+
+    def __init__(self, data, output_length=56):
+        self.data = torch.from_numpy(data).to(dtype=torch.float32)
+        self.output_length = output_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index, :-self.output_length], self.data[index, -self.output_length:]
+    
+batch_size = 32
+ecg_train_dataset = ECG5000Dataset(ecg_train)
+ecg_test_dataset = ECG5000Dataset(ecg_test)
+ecg_train_dataloader = DataLoader(ecg_train_dataset, batch_size=batch_size, shuffle=True)
+ecg_test_dataloader = DataLoader(ecg_test_dataset, batch_size=batch_size, shuffle=False)
+
+
+def train_model(net,loss_type, learning_rate, epochs=1000, gamma = 0.001,
+                print_every=50,eval_every=50, verbose=1, Lambda=1, alpha=0.5):
+    
+    optimizer = torch.optim.Adam(net.parameters(),lr=learning_rate)
+    criterion = torch.nn.MSELoss()
+    
+    for epoch in range(epochs): 
+        for i, data in enumerate(ecg_train_dataloader, 0):
+            inputs, target, _ = data
+            inputs = torch.tensor(inputs, dtype=torch.float32).to(device)
+            target = torch.tensor(target, dtype=torch.float32).to(device)
+            batch_size, N_output = target.shape[0:2]                     
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss_mse,loss_shape,loss_temporal = torch.tensor(0),torch.tensor(0),torch.tensor(0)
+            
+            if (loss_type=='mse'):
+                loss_mse = criterion(target,outputs)
+                loss = loss_mse                   
+ 
+            if (loss_type=='dilate'):    
+                loss, loss_shape, loss_temporal = dilate_loss(target,outputs,alpha, gamma, device)             
+                  
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()          
+        
+        if(verbose):
+            if (epoch % print_every == 0):
+                print('epoch ', epoch, ' loss ',loss.item(),' loss shape ',loss_shape.item(),' loss temporal ',loss_temporal.item())
+                eval_model(net,ecg_test_dataloader, gamma,verbose=1)
+  
+
+def eval_model(net,loader, gamma,verbose=1):   
+    criterion = torch.nn.MSELoss()
+    losses_mse = []
+    losses_dtw = []
+    losses_tdi = []   
+
+    for i, data in enumerate(loader, 0):
+        loss_mse, loss_dtw, loss_tdi = torch.tensor(0),torch.tensor(0),torch.tensor(0)
+        # get the inputs
+        inputs, target, breakpoints = data
+        inputs = torch.tensor(inputs, dtype=torch.float32).to(device)
+        target = torch.tensor(target, dtype=torch.float32).to(device)
+        batch_size, N_output = target.shape[0:2]
+        outputs = net(inputs)
+         
+        # MSE    
+        loss_mse = criterion(target,outputs)    
+        loss_dtw, loss_tdi = 0,0
+        # DTW and TDI
+        for k in range(batch_size):         
+            target_k_cpu = target[k,:,0:1].view(-1).detach().cpu().numpy()
+            output_k_cpu = outputs[k,:,0:1].view(-1).detach().cpu().numpy()
+
+            path, sim = dtw_path(target_k_cpu, output_k_cpu)   
+            loss_dtw += sim
+                       
+            Dist = 0
+            for i,j in path:
+                    Dist += (i-j)*(i-j)
+            loss_tdi += Dist / (N_output*N_output)            
+                        
+        loss_dtw = loss_dtw /batch_size
+        loss_tdi = loss_tdi / batch_size
+
+        # print statistics
+        losses_mse.append( loss_mse.item() )
+        losses_dtw.append( loss_dtw )
+        losses_tdi.append( loss_tdi )
+
+    print( ' Eval mse= ', np.array(losses_mse).mean() ,' dtw= ',np.array(losses_dtw).mean() ,' tdi= ', np.array(losses_tdi).mean()) 
+
+
+encoder = EncoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1, batch_size=batch_size).to(device)
+decoder = DecoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1,fc_units=16, output_size=1).to(device)
+net_gru_dilate = Net_GRU(encoder,decoder, N_output, device).to(device)
+train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.001, epochs=300, gamma=gamma, print_every=50, eval_every=50,verbose=1)
+
+encoder = EncoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1, batch_size=batch_size).to(device)
+decoder = DecoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1,fc_units=16, output_size=1).to(device)
+net_gru_mse = Net_GRU(encoder,decoder, N_output, device).to(device)
+train_model(net_gru_mse,loss_type='mse',learning_rate=0.001, epochs=300, gamma=gamma, print_every=50, eval_every=50,verbose=1)
+
+# Visualize results
+gen_test = iter(ecg_test_dataloader)
+test_inputs, test_targets, breaks = next(gen_test)
+
+test_inputs  = torch.tensor(test_inputs, dtype=torch.float32).to(device)
+test_targets = torch.tensor(test_targets, dtype=torch.float32).to(device)
+criterion = torch.nn.MSELoss()
+
+nets = [net_gru_mse,net_gru_dilate]
+if not os.path.exists('plots'):
+    os.makedirs('plots')
+for ind in range(1,51):
+    plt.figure()
+    plt.rcParams['figure.figsize'] = (17.0,5.0)  
+    k = 1
+    for net in nets:
+        pred = net(test_inputs).to(device)
+
+        input = test_inputs.detach().cpu().numpy()[ind,:,:]
+        target = test_targets.detach().cpu().numpy()[ind,:,:]
+        preds = pred.detach().cpu().numpy()[ind,:,:]
+
+        plt.subplot(1,3,k)
+        plt.plot(range(0,N_input) ,input,label='input',linewidth=3)
+        plt.plot(range(N_input-1,N_input+N_output), np.concatenate([ input[N_input-1:N_input], target ]) ,label='target',linewidth=3)   
+        plt.plot(range(N_input-1,N_input+N_output),  np.concatenate([ input[N_input-1:N_input], preds ])  ,label='prediction',linewidth=3)       
+        plt.xticks(range(0,40,2))
+        plt.legend()
+        k = k+1
+
+    plt.savefig('plots/dilate_shape_loss_500.png')  # Save figure
+    plt.close()
