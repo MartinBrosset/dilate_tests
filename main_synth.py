07e4d3d643d0bced0cbb4639fc66def4ae803b13
Martin Brosset <martin.brosset@student-cs.fr>
2023-12-14 13:09:56 +0100

    param 2

--- main_synth.py
+++ main_synth.py
@@ -145,7 +145,7 @@ def eval_model(net,loader, gamma,verbose=1):
 encoder = EncoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1, batch_size=batch_size).to(device)
 decoder = DecoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1,fc_units=16, output_size=1).to(device)
 net_gru_dilate = Net_GRU(encoder,decoder, N_output, device).to(device)
-train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.005, epochs=200, gamma=gamma, print_every=5, eval_every=5,verbose=1)
+train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.0005, epochs=200, gamma=gamma, print_every=5, eval_every=5,verbose=1)
 
 encoder = EncoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1, batch_size=batch_size).to(device)
 decoder = DecoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1,fc_units=16, output_size=1).to(device)
