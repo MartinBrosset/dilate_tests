f2e5cb864f895ba2cbdb1fddf5de315a5b64a28e
Martin Brosset <martin.brosset@student-cs.fr>
2023-12-14 14:16:07 +0100

    test alpha = 1

--- main_synth.py
+++ main_synth.py
@@ -77,7 +77,7 @@ trainloader = DataLoader(ecg_train_dataset, batch_size=batch_size, shuffle=True)
 testloader = DataLoader(ecg_test_dataset, batch_size=batch_size, shuffle=False)
 
 def train_model(net,loss_type, learning_rate, epochs=1000, gamma = 0.001,
-                print_every=50,eval_every=50, verbose=1, Lambda=1, alpha=0.5):
+                print_every=50,eval_every=50, verbose=1, Lambda=1, alpha=1):
     
     optimizer = torch.optim.Adam(net.parameters(),lr=learning_rate)
     criterion = torch.nn.MSELoss()
@@ -155,7 +155,7 @@ def eval_model(net,loader, gamma,verbose=1):
 encoder = EncoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1, batch_size=batch_size).to(device)
 decoder = DecoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1,fc_units=16, output_size=1).to(device)
 net_gru_dilate = Net_GRU(encoder,decoder, N_output, device).to(device)
-train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.001, epochs=200, gamma=gamma, print_every=5, eval_every=5,verbose=1)
+train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.0005, epochs=200, gamma=gamma, print_every=3, eval_every=3,verbose=1)
 
 encoder = EncoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1, batch_size=batch_size).to(device)
 decoder = DecoderRNN(input_size=1, hidden_size=128, num_grulstm_layers=1,fc_units=16, output_size=1).to(device)
