f9ed6d0747b3b9b6d7e57d163460a90c03e79c03
Martin Brosset <martin.brosset@student-cs.fr>
2023-12-18 08:45:28 +0100

    test autres param insect 3

--- main_insect.py
+++ main_insect.py
@@ -83,7 +83,7 @@ def train_model(net,loss_type, learning_rate, epochs=1000, gamma = 0.001,
     
     optimizer = torch.optim.Adam(net.parameters(),lr=learning_rate)
     ### learning rate adaptatif qui diminue au cours des epochs
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.8)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.8)
 
     criterion = torch.nn.MSELoss()
     
@@ -109,7 +109,7 @@ def train_model(net,loss_type, learning_rate, epochs=1000, gamma = 0.001,
             optimizer.step() 
 
         ### on ajoute un pas au scheduler pour mettre le learning rate à jour
-        #scheduler.step()
+        scheduler.step()
         
         if (epoch % print_every == 0):
             print('epoch ', epoch, ' loss ',loss.item(),' loss shape ',loss_shape.item(),' loss temporal ',loss_temporal.item())
@@ -164,7 +164,7 @@ def eval_model(net,loader, gamma,verbose=1):
 ### CREATION DU MODELE GRU (Seq2Seq) ET ENTRAINEMENT
 
 net_gru_dilate = Seq2Seq(input_size=1, hidden_size=128, num_layers=1, fc_units=16, output_size=1, target_length=N_output, device=device).to(device)
-train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.1, epochs=300, gamma=gamma, alpha=0.7, print_every=5)
+train_model(net_gru_dilate,loss_type='dilate',learning_rate=0.001, epochs=300, gamma=gamma, alpha=0.7, print_every=5)
 final_mse, final_dtw, final_tdi = eval_model(net_gru_dilate, testloader, gamma)
 
 net_gru_mse = Seq2Seq(input_size=1, hidden_size=128, num_layers=1, fc_units=16, output_size=1, target_length=N_output, device=device).to(device)
@@ -172,7 +172,7 @@ train_model(net_gru_mse,loss_type='mse',learning_rate=0.001, epochs=100, gamma=g
 final_mse_2, final_dtw_2, final_tdi_2 = eval_model(net_gru_mse, testloader, gamma)
 
 net_gru_soft_dtw = Seq2Seq(input_size=1, hidden_size=128, num_layers=1, fc_units=16, output_size=1, target_length=N_output, device=device).to(device)
-train_model(net_gru_soft_dtw,loss_type='dilate',learning_rate=0.1, epochs=200, gamma=gamma, alpha =1, print_every=5)
+train_model(net_gru_soft_dtw,loss_type='dilate',learning_rate=0.001, epochs=200, gamma=gamma, alpha =1, print_every=5)
 final_mse_3, final_dtw_3, final_tdi_3 = eval_model(net_gru_soft_dtw, testloader, gamma)
 
 
